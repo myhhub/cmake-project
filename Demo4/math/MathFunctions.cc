d389bfc8ab310a64384fb640a04a6f0920182948
myhhub <yh_ma@qq.com>
2021-03-29 09:02:03 +0800

    all

---
+++ Demo4/math/MathFunctions.cc
@@ -0,0 +1,22 @@
+/**
+ * power - Calculate the power of number.
+ * @param base: Base value.
+ * @param exponent: Exponent value.
+ *
+ * @return base raised to the power exponent.
+ */
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+
+    if (exponent == 0) {
+        return 1;
+    }
+    
+    for(i = 1; i < exponent; ++i){
+        result = result * base;
+    }
+
+    return result;
+}
